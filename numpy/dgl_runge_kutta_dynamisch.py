fix dynamic runge kutta calculation
add step by step output to sympy dynamic runge kutta

numpy/dgl_runge_kutta_dynamisch.py
@@ -4,24 +4,30 @@ import numpy as np
 # Definitionen          #
 #########################
 
-a = -1.5
-b = 1.5
-n = 5
+a = 2
+b = 5
+n = 1
 
-y0 = 0
-f = lambda x, y: x**2 + 0.1 * y
+y0 = 1
+f = lambda x, y: x / y
 
-p = 4
-
-c_a = np.array([
+# c_1 | a_11 a_12 ... a_1s
+# c_2 | a_21 a_22 ... a_2s
+# ... | ...
+# c_s | a_s1 a_s2 ... a_ss
+# ----|--------------------
+#     | b_1  b_2  ... b_s
+butcher = np.array([
     [0, 0, 0, 0],
-    [0.5, 0, 0, 0],
-    [0, 0.5, 0, 0],
-    [0, 0, 1, 0]
+    [1/3, 1/3, 0, 0],
+    [2/3, 0, 2/3, 0],
+    [0, 1/4, 0, 3/4]
 ])
 
-c_b = np.array([1 / 6, 1 / 3, 1 / 3, 1 / 6])
-c_c = np.array([0, 0.5, 0.5, 1])
+s = butcher.shape[0] - 1
+c_a = butcher[:-1, 1:]
+c_b = butcher[-1, 1:]
+c_c = butcher[:-1, 0]
 
 #########################
 # Runge-Kutta Verfahren #
@@ -29,18 +35,18 @@ c_c = np.array([0, 0.5, 0.5, 1])
 
 step_size = (b - a) / n
 
-x = np.linspace(a, b, n + 1)
-y = np.zeros(n + 1)
-y[0] = y0
+x = [a]
+y = [y0]
 
 for i in range(n):
-    k = np.zeros(p)
+    k = np.zeros(s)
+    k[0] = f(x[-1], y[-1])
 
-    k[0] = f(x[i], y[i])
-    for j in range(1, p):
-        k[j] = f(x[i] + step_size * c_c[j], y[i] +
-                 step_size * np.sum(c_a[j, :j] * k[:j]))
+    for j in range(1, s):
+        x_j = x[-1] + c_c[j] * step_size
+        y_j = y[-1] + step_size * np.sum(c_a[j, :j] * k[:j])
+        k[j] = f(x_j, y_j)
 
-    y[i + 1] = y[i] + step_size * np.sum(c_b * k)
+    y.append(y[-1] + step_size * np.sum(c_b * k))
 
 print(y)

sympy/dgl_runge_kutta.py
@@ -57,14 +57,46 @@ def dynamic(f, a, b, n, y0,
         y = y_list[-1]
 
         k = np.zeros(intermediate_steps)
+
         k[0] = f(x, y)
+        if print_steps:
+            dp.display(dp.Markdown(f"#### Iteration {i + 1}"))
+            k0_str = f"k_0 = f(x_{{{i}}}, y_{{{i}}}) = f({x:.{precision}f}, {y:.{precision}f}) = {f(x, y):.{precision}f}"
+            dp.display(dp.Math(k0_str))
 
         for j in range(1, intermediate_steps):
-            k[j] = f(x + intermediate_step_factors[j] * h, y + h *
-                     np.sum(previous_intermediate_weights[j, :j] * k[:j]))
+            x_j = x + intermediate_step_factors[j] * h
+            y_j = y + h * np.sum(previous_intermediate_weights[j, :j] * k[:j])
+            k[j] = f(x_j, y_j)
+            
+            if print_steps:
+                sum_str = '\\left['
+                for m in range(j):
+                    sum_str += f"{previous_intermediate_weights[j, m]:.{precision}f} \\cdot {k[m]:.{precision}f}"
+                    if m != j - 1:
+                        sum_str += ' + '
+                sum_str += '\\right]'
+
+                k_str = f"k_{{{j}}} = f(x_{{{i}}} + c_{{{j}}} \\cdot h, y_{{{i}}} + h \\cdot \\sum_{{m = 0}}^{{{j - 1}}} c_{{m, {j}}} \\cdot k_m)"
+                k_str += f" = f({x:.{precision}f} + {intermediate_step_factors[j]} \\cdot {h}, {y:.{precision}f} + {h} \\cdot {sum_str})"
+                k_str += f" = f({x_j:.{precision}f}, {y_j:.{precision}f}) = {k[j]:.{precision}f}"
+                dp.display(dp.Math(k_str))
 
         x_next = x + h
         y_next = y + h * np.sum(intermediate_weights * k)
+
+        if print_steps:
+            sum_str = '\\left['
+            for m in range(intermediate_steps):
+                sum_str += f"{intermediate_weights[m]:.{precision}f} \\cdot {k[m]:.{precision}f}"
+                if m != intermediate_steps - 1:
+                    sum_str += ' + '
+            sum_str += '\\right]'
+
+            x_next_str = f"x_{{{i + 1}}} = x_{{{i}}} + h = {x:.{precision}f} + {h} = {x_next:.{precision}f}"
+            y_next_str = f"y_{{{i + 1}}} = y_{{{i}}} + h \\cdot \\sum_{{m = 0}}^{{{intermediate_steps - 1}}} b_m \\cdot k_m = {y:.{precision}f} + {h} \\cdot {sum_str} = {y_next:.{precision}f}"
+            dp.display(dp.Math(
+                f"\\begin{{aligned}}& {x_next_str} \\\\ & {y_next_str} \\end{{aligned}}"))
 
         x_list.append(x_next)
         y_list.append(y_next)