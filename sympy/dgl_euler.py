25a76b69dbebcddc6d142e2ca176a3ff29c5b9a9
Florian Walter <nairolf.retlaw@gmail.com>
2023-06-19 11:22:21 +0200

    remove precision format in dgl sympy scripts to fix output of vectors

--- sympy/dgl_euler.py
+++ sympy/dgl_euler.py
@@ -1,7 +1,8 @@
+import sympy as sp
 import IPython.display as dp
 
 
-def classic(f, a, b, n, y0, precision=4, print_steps=False):
+def classic(f, a, b, n, y0, print_steps=False):
     h = (b - a) / n
 
     x_list = [a]
@@ -15,8 +16,8 @@ def classic(f, a, b, n, y0, precision=4, print_steps=False):
         x_next = x + h
 
         if print_steps:
-            x_next_str = f"x_{{{i + 1}}} = x_{{{i}}} + h = {x:.{precision}f} + {h} = {x_next:.{precision}f}"
-            y_next_str = f"y_{{{i + 1}}} = y_{{{i}}} + h \\cdot f(x_{{{i}}}, y_{{{i}}}) = {y:.{precision}f} + {h} \\cdot {f(x, y):.{precision}f} = {y_next:.{precision}f}"
+            x_next_str = f"x_{{{i + 1}}} = x_{{{i}}} + h = {sp.latex(x)} + {h} = {sp.latex(x_next)}"
+            y_next_str = f"y_{{{i + 1}}} = y_{{{i}}} + h \\cdot f(x_{{{i}}}, y_{{{i}}}) = {sp.latex(y)} + {h} \\cdot {sp.latex(f(x, y))} = {sp.latex(y_next)}"
             dp.display(dp.Math(x_next_str + " \\quad " + y_next_str))
 
         x_list.append(x_next)
@@ -25,7 +26,7 @@ def classic(f, a, b, n, y0, precision=4, print_steps=False):
     return x_list, y_list
 
 
-def midpoint(f, a, b, n, y0, precision=4, print_steps=False):
+def midpoint(f, a, b, n, y0, print_steps=False):
     h = (b - a) / n
 
     x_list = [a]
@@ -39,16 +40,16 @@ def midpoint(f, a, b, n, y0, precision=4, print_steps=False):
         y_mid = y + h / 2 * f(x, y)
 
         if print_steps:
-            x_mid_str = f"x_{{{i + 0.5}}} = x_{{{i}}} + \\frac{{h}}{{2}} = {x:.{precision}f} + \\frac{{{h}}}{{2}} = {x_mid:.{precision}f}"
-            y_mid_str = f"y_{{{i + 0.5}}} = y_{{{i}}} + \\frac{{h}}{{2}} \\cdot f(x_{{{i}}}, y_{{{i}}}) = {y:.{precision}f} + \\frac{{{h}}}{{2}} \\cdot {f(x, y):.{precision}f} = {y_mid:.{precision}f}"
+            x_mid_str = f"x_{{{i + 0.5}}} = x_{{{i}}} + \\frac{{h}}{{2}} = {sp.latex(x)} + \\frac{{{h}}}{{2}} = {sp.latex(x_mid)}"
+            y_mid_str = f"y_{{{i + 0.5}}} = y_{{{i}}} + \\frac{{h}}{{2}} \\cdot f(x_{{{i}}}, y_{{{i}}}) = {sp.latex(y)} + \\frac{{{h}}}{{2}} \\cdot {sp.latex(f(x, y))} = {sp.latex(y_mid)}"
             dp.display(dp.Math(x_mid_str + " \\quad " + y_mid_str))
 
         y_next = y + h * f(x_mid, y_mid)
         x_next = x + h
 
         if print_steps:
-            x_next_str = f"x_{{{i + 1}}} = x_{{{i}}} + h = {x:.{precision}f} + {h} = {x_next:.{precision}f}"
-            y_next_str = f"y_{{{i + 1}}} = y_{{{i}}} + h \\cdot f(x_{{{i + 0.5}}}, y_{{{i + 0.5}}}) = {y:.{precision}f} + {h} \\cdot f({x_mid:.{precision}f}, {y_mid:.{precision}f}) = {y:.{precision}f} + {h} \\cdot {f(x_mid, y_mid):.{precision}f} = {y_next:.{precision}f}"
+            x_next_str = f"x_{{{i + 1}}} = x_{{{i}}} + h = {sp.latex(x)} + {h} = {sp.latex(x_next)}"
+            y_next_str = f"y_{{{i + 1}}} = y_{{{i}}} + h \\cdot f(x_{{{i + 0.5}}}, y_{{{i + 0.5}}}) = {sp.latex(y)} + {h} \\cdot f \\left({sp.latex(x_mid)}, {sp.latex(y_mid)} \\right) = {sp.latex(y)} + {h} \\cdot {sp.latex(f(x_mid, y_mid))} = {sp.latex(y_next)}"
             dp.display(dp.Math(x_next_str + " \\quad " + y_next_str))
 
         y_list.append(y_next)
@@ -57,7 +58,7 @@ def midpoint(f, a, b, n, y0, precision=4, print_steps=False):
     return x_list, y_list
 
 
-def modified(f, a, b, n, y0, precision=4, print_steps=False):
+def modified(f, a, b, n, y0, print_steps=False):
     h = (b - a) / n
 
     x_list = [a]
@@ -71,16 +72,16 @@ def modified(f, a, b, n, y0, precision=4, print_steps=False):
         k2 = f(x + h, y + h * k1)
 
         if print_steps:
-            k1_str = f"k_1 = f(x_{{{i}}}, y_{{{i}}}) = {f(x, y):.{precision}f}"
-            k2_str = f"k_2 = f(x_{{{i}}} + h, y_{{{i}}} + h \\cdot k_1) = f({x:.{precision}f} + {h}, {y:.{precision}f} + {h} \\cdot {k1:.{precision}f}) = {f(x + h, y + h * k1):.{precision}f}"
+            k1_str = f"k_1 = f(x_{{{i}}}, y_{{{i}}}) = {sp.latex(f(x, y))}"
+            k2_str = f"k_2 = f(x_{{{i}}} + h, y_{{{i}}} + h \\cdot k_1) = f \\left({sp.latex(x)} + {h}, {sp.latex(y)} + {h} \\cdot {sp.latex(k1)} \\right) = {sp.latex(f(x + h, y + h * k1))}"
             dp.display(dp.Math(k1_str + " \\quad " + k2_str))
 
         y_next = y + h * (k1 + k2) / 2
         x_next = x + h
 
         if print_steps:
-            x_next_str = f"x_{{{i + 1}}} = x_{{{i}}} + h = {x:.{precision}f} + {h} = {x_next:.{precision}f}"
-            y_next_str = f"y_{{{i + 1}}} = y_{{{i}}} + \\frac{{h}}{{2}} \\cdot (k_1 + k_2) = {y:.{precision}f} + \\frac{{{h}}}{{2}} \\cdot ({k1:.{precision}f} + {k2:.{precision}f}) = {y_next:.{precision}f}"
+            x_next_str = f"x_{{{i + 1}}} = x_{{{i}}} + h = {sp.latex(x)} + {h} = {sp.latex(x_next)}"
+            y_next_str = f"y_{{{i + 1}}} = y_{{{i}}} + \\frac{{h}}{{2}} \\cdot (k_1 + k_2) = {sp.latex(y)} + \\frac{{{h}}}{{2}} \\cdot \\left( {sp.latex(k1)} + {sp.latex(k2)} \\right) = {sp.latex(y_next)}"
             dp.display(dp.Math(x_next_str + " \\quad " + y_next_str))
 
         y_list.append(y_next)
--- sympy/dgl_runge_kutta.py
+++ sympy/dgl_runge_kutta.py
@@ -1,8 +1,9 @@
 import numpy as np
+import sympy as sp
 import IPython.display as dp
 
 
-def classic(f, a, b, n, y0, precision=4, print_steps=False):
+def classic(f, a, b, n, y0, print_steps=False):
     h = (b - a) / n
 
     x_list = [a]
@@ -23,15 +24,15 @@ def classic(f, a, b, n, y0, precision=4, print_steps=False):
         if print_steps:
             dp.display(dp.Markdown(f"#### Iteration {i + 1}"))
 
-            k1_str = f"k_1 = f(x_{{{i}}}, y_{{{i}}}) = {f(x, y):.{precision}f}"
-            k2_str = f"k_2 = f(x_{{{i}}} + \\frac{{h}}{{2}}, y_{{{i}}} + \\frac{{h}}{{2}} \\cdot k_1) = f({x:.{precision}f} + \\frac{{{h}}}{{2}}, {y:.{precision}f} + \\frac{{{h}}}{{2}} \\cdot {k1:.{precision}f}) = {f(x + h/2, y + h/2 * k1):.{precision}f}"
-            k3_str = f"k_3 = f(x_{{{i}}} + \\frac{{h}}{{2}}, y_{{{i}}} + \\frac{{h}}{{2}} \\cdot k_2) = f({x:.{precision}f} + \\frac{{{h}}}{{2}}, {y:.{precision}f} + \\frac{{{h}}}{{2}} \\cdot {k2:.{precision}f}) = {f(x + h/2, y + h/2 * k2):.{precision}f}"
-            k4_str = f"k_4 = f(x_{{{i}}} + h, y_{{{i}}} + h \\cdot k_3) = f({x:.{precision}f} + {h}, {y:.{precision}f} + {h} \\cdot {k3:.{precision}f}) = {f(x + h, y + h * k3):.{precision}f}"
+            k1_str = f"k_1 = f(x_{{{i}}}, y_{{{i}}}) = {sp.latex(f(x, y))}"
+            k2_str = f"k_2 = f(x_{{{i}}} + \\frac{{h}}{{2}}, y_{{{i}}} + \\frac{{h}}{{2}} \\cdot k_1) = f({sp.latex(x)} + \\frac{{{h}}}{{2}}, {sp.latex(y)} + \\frac{{{h}}}{{2}} \\cdot {sp.latex(k1)}) = {sp.latex(f(x + h/2, y + h/2 * k1))}"
+            k3_str = f"k_3 = f(x_{{{i}}} + \\frac{{h}}{{2}}, y_{{{i}}} + \\frac{{h}}{{2}} \\cdot k_2) = f({sp.latex(x)} + \\frac{{{h}}}{{2}}, {sp.latex(y)} + \\frac{{{h}}}{{2}} \\cdot {sp.latex(k2)}) = {sp.latex(f(x + h/2, y + h/2 * k2))}"
+            k4_str = f"k_4 = f(x_{{{i}}} + h, y_{{{i}}} + h \\cdot k_3) = f({sp.latex(x)} + {h}, {sp.latex(y)} + {h} \\cdot {sp.latex(k3)}) = {sp.latex(f(x + h, y + h * k3))}"
             dp.display(dp.Math(
                 f"\\begin{{aligned}}& {k1_str} \\\\ & {k2_str} \\\\ & {k3_str} \\\\ & {k4_str} \\end{{aligned}}"))
 
-            x_next_str = f"x_{{{i + 1}}} = x_{{{i}}} + h = {x:.{precision}f} + {h} = {x_next:.{precision}f}"
-            y_next_str = f"y_{{{i + 1}}} = y_{{{i}}} + \\frac{{h}}{{6}} \\cdot (k_1 + 2 \\cdot k_2 + 2 \\cdot k_3 + k_4) = {y:.{precision}f} + \\frac{{{h}}}{{6}} \\cdot ({k1:.{precision}f} + 2 \\cdot {k2:.{precision}f} + 2 \\cdot {k3:.{precision}f} + {k4:.{precision}f}) = {y_next:.{precision}f}"
+            x_next_str = f"x_{{{i + 1}}} = x_{{{i}}} + h = {sp.latex(x)} + {h} = {sp.latex(x_next)}"
+            y_next_str = f"y_{{{i + 1}}} = y_{{{i}}} + \\frac{{h}}{{6}} \\cdot (k_1 + 2 \\cdot k_2 + 2 \\cdot k_3 + k_4) = {sp.latex(y)} + \\frac{{{h}}}{{6}} \\cdot ({sp.latex(k1)} + 2 \\cdot {sp.latex(k2)} + 2 \\cdot {sp.latex(k3)} + {sp.latex(k4)}) = {sp.latex(y_next)}"
             dp.display(dp.Math(
                 f"\\begin{{aligned}}& {x_next_str} \\\\ & {y_next_str} \\end{{aligned}}"))
 
